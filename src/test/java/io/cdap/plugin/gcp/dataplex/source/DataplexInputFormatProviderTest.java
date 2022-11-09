247d6af051a03d6e8e4c510f0c53324ca899e5f7
Shubhangi-cs <shubhangi.garg@cloudsufi.com>
2022-11-09 13:33:43 +0530

    Dataplex JUnits

---
+++ src/test/java/io/cdap/plugin/gcp/dataplex/source/DataplexInputFormatProviderTest.java
@@ -0,0 +1,105 @@
+package io.cdap.plugin.gcp.dataplex.source;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.dataplex.v1.StorageSystem;
+import io.cdap.plugin.common.batch.ConfigurationUtils;
+import io.cdap.plugin.gcp.common.GCPUtils;
+import io.cdap.plugin.gcp.dataplex.common.util.DataplexConstants;
+import io.cdap.plugin.gcp.dataplex.common.util.DataplexUtil;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.InputFormat;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Spliterator;
+import java.util.stream.StreamSupport;
+
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ConfigurationUtils.class, GCPUtils.class, GoogleCredentials.class, DataplexUtil.class})
+public class DataplexInputFormatProviderTest {
+
+  @Mock
+  JobContext mockContext;
+
+  @Mock
+  private Configuration configuration;
+
+  @Mock
+  private InputFormat inputFormat;
+
+  @Mock
+  private InputSplit inputSplit;
+
+  @Mock
+  private TaskAttemptContext context;
+
+  @Mock
+  private ConfigurationUtils configurationUtils;
+
+  @Test
+  public void testGetInputFormatClassName() throws Exception {
+    Configuration configuration = mock(Configuration.class);
+    PowerMockito.mockStatic(StreamSupport.class);
+    String entityType = StorageSystem.CLOUD_STORAGE.toString();
+    when(configuration.get(DataplexConstants.DATAPLEX_ENTITY_TYPE)).thenReturn(entityType);
+    PowerMockito.mockStatic(ConfigurationUtils.class);
+    DataplexInputFormatProvider.DataplexInputFormat dataInputFormat = new
+      DataplexInputFormatProvider.DataplexInputFormat();
+    Map<String, String> map = new HashMap<>();
+    map.put("price", "100");
+    Mockito.when(ConfigurationUtils.getNonDefaultConfigurations(configuration)).thenReturn(map);
+    DataplexInputFormatProvider dataplexInputFormatProvider = new DataplexInputFormatProvider(configuration);
+    Assert.assertThrows(ClassCastException.class, () -> {
+      dataInputFormat.createRecordReader(inputSplit, context);
+    });
+    Assert.assertEquals("io.cdap.plugin.gcp.dataplex.source.DataplexInputFormatProvider" +
+      "$DataplexInputFormat", dataplexInputFormatProvider.getInputFormatClassName());
+  }
+
+  @Test
+  public void testGetInputFormatDataplexClassNameWithBigquery() throws Exception {
+    Configuration configuration = mock(Configuration.class);
+    Map<String, String> inputFormatConfiguration = new HashMap<>();
+    DataplexInputFormatProvider.DataplexInputFormat dataplexInputFormat = new
+      DataplexInputFormatProvider.DataplexInputFormat();
+    inputFormatConfiguration.put("key", "value");
+    PowerMockito.mockStatic(StreamSupport.class);
+    Spliterator spliterator = Mockito.mock(Spliterator.class);
+    String entityType = "BIGQUERY";
+    when(configuration.get(DataplexConstants.DATAPLEX_ENTITY_TYPE)).thenReturn(entityType);
+    when(configuration.spliterator()).thenReturn(spliterator);
+    DataplexInputFormatProvider dataplexInputFormatProvider = new DataplexInputFormatProvider(configuration);
+    dataplexInputFormatProvider.getInputFormatConfiguration();
+    Assert.assertThrows(IOException.class, () -> {
+      dataplexInputFormat.getSplits(mockContext);
+    });
+    Assert.assertEquals("io.cdap.plugin.gcp.bigquery.source.PartitionedBigQueryInputFormat",
+      dataplexInputFormatProvider.getInputFormatClassName());
+  }
+
+  @Test
+  public void testGetInputFormatNotDataPlexClassNameNull() throws Exception {
+    Configuration configuration = mock(Configuration.class);
+    String entityType = "entity";
+    when(configuration.get(DataplexConstants.DATAPLEX_ENTITY_TYPE)).thenReturn(entityType);
+    DataplexInputFormatProvider dataplexInputFormatProvider = new DataplexInputFormatProvider(configuration);
+    Assert.assertNull(dataplexInputFormatProvider.getInputFormatClassName());
+  }
+
+}
